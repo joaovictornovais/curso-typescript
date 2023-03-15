90a39d48ad2ae3fed1b7a825fd8e630b45b28f59
João Victor <joao.dipaay@gmail.com>
2023-03-15 15:24:37 -0300

    Typeanotations

---
+++ src/A001-typeanotation/A001-typeanotation.ts
@@ -0,0 +1,26 @@
+/* eslint-disable */
+
+// Tipos básicos, aonde ocorrem inferência de tipos.
+let nome: string = 'João'; // Qualquer tipo de string
+let idade: number = 12; // 10, 1.57, -5.55, 0xf00d, 0b1010, 0o7744
+let adulto: boolean = true; // true ou false
+let simbolo: symbol = Symbol('Qualquer simoblo'); // symbol
+// let big: bigint = 10n; // bigint
+
+// Arrays
+let arrayDeNumeros: Array<Number> = [1, 2, 3];
+let arrayDeNumeros2: Number[] = [4, 5, 6]
+let arrayDeString: Array<String> = ['a', 'b']
+let arrayDeString2: String[] = ['c', 'd']
+
+// Objetos
+let pessoa: {nome: string, idade: number, adulto?: boolean} = {
+    idade: 19,
+    nome: 'João',
+};
+
+// Funções
+function soma(x: number, y: number) {
+    return soma + 2
+};
+const soma2: (x: number, y: number) => number = (x, y) => x + y
