e9b9177e9b85dc461e6a284831ac6486b5024677
João Victor <joao.dipaay@gmail.com>
2023-03-16 11:02:13 -0300

    Exercicio resumo

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-import funcao from './mod';
+const carro: { gastoPorLitro: number; litroGasolina: number; km: number } = {
+  gastoPorLitro: 6.5,
+  litroGasolina: 40,
+  km: 261,
+};
 
-funcao();
+function rodaOuNaoRoda(
+  gastoPorLitro: number,
+  litroGasolina: number,
+  km: number,
+): string {
+  const formula = gastoPorLitro * litroGasolina;
+  if (km < formula) {
+    return `Você consegue rodar esses ${km}km's e sobrará ${
+      formula - km
+    }L de gasolina`;
+  } else
+    return `Você não consegue rodar ${km}km com ${litroGasolina}L de gasolina. Abasteça mais ${
+      (km - formula) * gastoPorLitro
+    }L`;
+}
+
+console.log(rodaOuNaoRoda(carro.gastoPorLitro, carro.litroGasolina, carro.km));
