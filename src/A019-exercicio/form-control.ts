5cbc6b4ec0a092bd82d12cf3c5259b62dc8954f9
João Victor <joao.dipaay@gmail.com>
2023-03-17 10:43:54 -0300

    This em arrow function

--- src/A019-exercicio/form-control.ts
+++ src/A019-exercicio/form-control.ts
@@ -8,14 +8,17 @@ const email = document.querySelector('.email') as HTMLInputElement;
 const password = document.querySelector('.password') as HTMLInputElement;
 const password2 = document.querySelector('.password2') as HTMLInputElement;
 
-form.addEventListener('submit', function (event) {
+const submitEventFn = (event: Event) => {
   event.preventDefault();
-  hideErrorMessages(this);
+  const target = event.target as HTMLFormElement;
+  hideErrorMessages(target);
   checkForEmptyFields(username, email, password, password2);
   checkEmail(email);
   checkEqualPassword(password, password2);
-  if (shouldSendForm(this)) console.log('FORMULÁRIO ENVIADO');
-});
+  if (shouldSendForm(target)) console.log('FORMULÁRIO ENVIADO');
+};
+
+form.addEventListener('submit', submitEventFn);
 
 function checkForEmptyFields(...inputs: HTMLInputElement[]): void {
   inputs.forEach((input) => {
---
+++ src/A020-exercicio/form-control.ts
@@ -0,0 +1,6 @@
+export function funcao(this: Date, name: string, age: number): void {
+  console.log(this);
+  console.log(name, age);
+}
+
+funcao.call(new Date(), 'Luiz', 30);
